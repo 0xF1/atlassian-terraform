ab969547ac643893f0cf1cd5395bd0779207f385
hooperp <phooper0001@yahoo.com>
2016-10-26 16:33:42 +0100

    Update variables.tf

--- atlassian-terraform/variables.tf
+++ atlassian-terraform/variables.tf
@@ -11,11 +11,11 @@ variable "aws_key_name" {
 
 variable "aws_access_key" {
   description = "AWS access_key for connection."
-  default = "AKIAIWUAE6GRAJD2KYUQ"
+  default = ""
 }
 
 variable "aws_secret_key" {
   description = "AWS secret_key for connection."
-  default = "N7klvU8RouKJCj6LnigowEKOof9M2WMRfg4Bjmr9"
+  default = ""
 }
 
